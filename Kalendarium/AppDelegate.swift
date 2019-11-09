793e388ec11adbede2502cc51fc70c43e37f37f9
Mike Manzo <manzo.mike@gmail.com>
2019-11-09 12:25:46 -0500

    Initial Commit

---
+++ Kalendarium/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kalendarium
+//
+//  Created by Mike Manzo on 11/9/19.
+//  Copyright © 2019 Mike Manzo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Kalendarium/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kalendarium
+//
+//  Created by Mike Manzo on 11/9/19.
+//  Copyright © 2019 Mike Manzo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
